b30a39b8addde84de3471f3d94aaec71c96b49a0
Osman Pérez <operez@edutecno.com>
2024-11-19 20:42:32 -0400

    Listados usando relaciones

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const conexion = require("./conexion.js");
 const Producto = require("./models/Producto.entity.js");
+const TipoProducto = require("./models/TipoProducto.entity.js");
 
 
 // Ejemplo Asíncrono
@@ -12,10 +13,55 @@ const Producto = require("./models/Producto.entity.js");
     try {
         await conexion.sync();
         console.log("Conexión exitosa");
+        // Creación de tipos de productos.
+        // const tipoRopa = await TipoProducto.create({ nombre: "Ropa" });
+        // const tipoAlimento = await TipoProducto.create({ nombre: "Alimentos" });
+        // console.log("Tipos de productos creados con éxito");
 
-        const listadoProductos = await Producto.findAll();
+        // Creación de productos.
+        // await Producto.create({ nombre: "Polera", precio: 13000, tipo_producto_id: tipoRopa.id});
+        // await Producto.create({ nombre: "Jean", precio: 20000, tipo_producto_id: tipoRopa.id});
+        // await Producto.create({ nombre: "Arroz", precio: 1500, tipo_producto_id: tipoAlimento.id});
+        // console.log("Productos creados con éxito");
 
-        console.log(listadoProductos);
+
+
+        // const listadoProductos = await Producto.findAll({
+        //     where: {
+        //         tipo_producto_id: 1
+        //     },
+        //     order: [
+        //         ["precio", "DESC"]
+        //     ],
+        //     attributes: ["id","nombre","precio"],
+        //     include: {
+        //         model: TipoProducto,
+        //         as: "tipoProducto",
+        //         attributes: ["id"]
+        //     }
+        // });
+        // console.table(JSON.parse(JSON.stringify(listadoProductos)));
+
+
+
+        // const listadoTipos = await TipoProducto.findAll({
+        //     include:{
+        //         model: Producto,
+        //         as: "productos"
+        //     }
+        // });
+
+        const listadoTipos = await TipoProducto.findOne({
+            where: {
+                id: 2
+            },
+            include:{
+                model: Producto,
+                as: "productos"
+            }
+        });
+        console.log(JSON.parse(JSON.stringify(listadoTipos)));
+
     } catch (error) {
         console.log(error.message);
     }
